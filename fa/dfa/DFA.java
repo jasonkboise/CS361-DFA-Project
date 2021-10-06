683bf82392f948f0fbcbca77aff86d3eb7d3a872
jasonkboise <54860002+jasonkboise@users.noreply.github.com>
2021-10-05 18:37:41 -0600

    Update DFA.java

--- fa/dfa/DFA.java
+++ fa/dfa/DFA.java
@@ -1,66 +1,59 @@
 package fa.dfa;
 
+import java.util.HashSet;
 import java.util.Set;
-
 import fa.State;
 
 public class DFA implements DFAInterface{
 
-    public DFA() {
+    private Set<DFAState> states;
 
+    public DFA() {
+        states = new HashSet<DFAState>();
     }
     @Override
     public void addStartState(String name) {
-        // TODO Auto-generated method stub
 
         
     }
     @Override
     public void addState(String name) {
-        // TODO Auto-generated method stub
         
     }
     @Override
     public void addFinalState(String name) {
-        // TODO Auto-generated method stub
         DFAState newState = new DFAState(name);
         newState.setFinal();
         //add it to the list of other states
+        states.add(newState);
 
     }
     @Override
     public void addTransition(String fromState, char onSymb, String toState) {
-        // TODO Auto-generated method stub
         
     }
     @Override
     public Set<? extends State> getStates() {
-        // TODO Auto-generated method stub
         return null;
     }
     @Override
     public Set<? extends State> getFinalStates() {
-        // TODO Auto-generated method stub
         return null;
     }
     @Override
     public State getStartState() {
-        // TODO Auto-generated method stub
         return null;
     }
     @Override
     public Set<Character> getABC() {
-        // TODO Auto-generated method stub
         return null;
     }
     @Override
     public fa.dfa.State getToState(DFAState from, char onSymb) {
-        // TODO Auto-generated method stub
         return null;
     }
     @Override
     public boolean accepts(String s) {
-        // TODO Auto-generated method stub
         return false;
     }
 }
